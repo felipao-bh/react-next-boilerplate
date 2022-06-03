4b04df21665cc960c1a0041ae06f7f886d1a3439
Felipe Gonçalves Ferreira <fferreira@sga.pucminas.br>
2022-06-03 19:51:26 -0300

    #1 Configurando um Git Hook c/ Husky e Lint-Staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const bla = 'var'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
